d09f5b51eb923313285eabbaca44f49904bbb20d
NoMeeseeks <Xavier.e.santamaria.ramirez@gmail.com>
2024-03-23 19:54:02 -0500

    fin de la seccion layout y rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    {
+      path: 'auth',
+      loadChildren: () => import('./auth/auth.module').then(m => AuthModule),
+    },
+    {
+      path: 'heroes',
+      loadChildren: () => import('./heroes/heroes.module').then(m => HeroesModule),
+    },
+    {
+      path: '404',
+      component: Error404PageComponent
+    },
+    {
+      path: '',
+      redirectTo: 'heroes',
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      redirectTo: '404'
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-const routes: Route[] = [];
+import path from 'path';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+const routes: Route[] =
+  [
+    {
+      path: '',
+      component: LayoutPagesComponent,
+      children:
+        [
+          {
+            path: 'login',
+            component: LoginPageComponent
+          },
+          {
+            path: 'new-account',
+            component: RegisterPageComponent
+          },
+          {
+            path: '**',
+            redirectTo: 'login'
+          }
+        ]
+    }
+  ];
 @NgModule({
   imports:
     [
