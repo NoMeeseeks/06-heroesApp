379a1f003c505625bb404d07d91d2f5acb9a202d
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-05 21:29:04 -0500

    feature Auth service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  private baseUrl = environment.baseUrl;
+
+  private user?: User;
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  get currentUser(): User | undefined {
+    if (!this.user) { return undefined };
+
+    //estructure clone
+    //basicamente la solucion para hacer un clone profundo
+    return structuredClone(this.user);
+  }
+
+  login(email: string, pass: string): Observable<User> {
+
+    //http.post('login',{email,pass})
+    return this.httpClient.get<User>(`${this.baseUrl}/users?id=1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('toke', user.id.toString()))
+      )
+
+  }
+}
